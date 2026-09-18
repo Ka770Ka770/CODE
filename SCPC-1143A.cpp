89cac687163ee530a0b921414b928df465aed7d3
Ka770 <Ka770_Zhang_Jianbin@outlook.com>
2026-09-18 21:48:57 +0800

    更新

--- SCPC-1143A.cpp
+++ SCPC-1143A.cpp
@@ -1,59 +1,34 @@
-#include<bits/stdc++.h>
-using namespace std;
-int a[10];
+#include<stdio.h>
+#include<string.h>
+char arr[10];
 int main(){
-    ios::sync_with_stdio(false);
-    cin.tie(0);cout.tie(0);
-    int t;
-    cin>>t;
-    for(int i=0;i<t;i++){
-        int n;
-        cin>>n;
-            for(int j=0;j<n;j++){
-                string s;
-                cin>>s;
-                for(int k=s.size()-1;k>=0;k--){
-                    if(s[k]<='9'&&'0'<=s[k]){
-                        a[s[k]-'0']++; //桶
-                        k=-1;
-                    }
-                }
-            }
-           	bool p=0;
-           	for(int k=0;k<5;k++){
-               	if(a[1]==0&&a[6]==0){
-               		cout<<"NO"<<'\n';     //一当有天不行就NO
-               		p=1;
-               		break;
-            	}
-            else if(a[2]==0&&a[7]==0){
-            		cout<<"NO"<<'\n';
-                	p=1;
-                	break;
-            	}
-            else if(a[3]==0&&a[8]==0){
-                	cout<<"NO"<<'\n';
-                	p=1;
-                	break;
-           		}
-      		else if(a[4]==0&&a[9]==0){
-         			cout<<"NO"<<'\n';             
-        			p=1;
-                	break;
-         		}
-         	else if(a[5]==0&&a[0]==0){
-        			cout<<"NO"<<'\n';  
-        			p=1;
-                	break;
-            	}
-        	}
-            if(p!=1){
-                cout<<"YES"<<'\n';
-            }
-            for(int j=0;j<10;j++){
-                a[j]=0;//桶归零
-            }
-        
+		int t;
+scanf("%d",&t);
+for(int i=0;i<t;i++){
+    int n;
+    scanf("%d",&n);
+    int last_num[5]={0};// 记录5个限行分组
+    int flag=1;       
+    for(int j=0;j<n;j++){
+        scanf("%s",arr);
+        int k=4;
+        for(;k>=0;k--){
+            if(arr[k]>='0'&&arr[k]<='9')break;
+        }
+        last_num[(arr[k]-'0')%5]++;
+    }     
+    // 检查覆盖的限行分组数量
+    int group_count = 0;
+    for(int k=0;k<5;k++){
+        if(last_num[k] > 0){
+            group_count++;
+        }
+    }        
+    // 只要覆盖至少2个不同的限行分组，就能每天开车
+    if(group_count >= 2) {
+        printf("yes\n");
+    } else {
+        printf("no\n");
     }
-    return 0;
 }
+return 0;}
